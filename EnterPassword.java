cb3201750c9750cb91e7505d321aeb6bb5996a79
oleg_kharebov <xarebov1993@yandex.ru>
2022-05-08 13:18:17 +0300

    first commit

---
+++ EnterPassword.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class EnterPassword {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите имя");
+        String enterName = scanner.nextLine();
+        System.out.println("Введите пароль");
+        String enterPassword = scanner.nextLine();
+        if (enterPassword.length() < 8 || enterPassword.length() > 15) {
+            do {
+                System.out.println("Длина пароля должна составлять от 8 до 15 символов");
+                enterPassword = scanner.nextLine();
+            } while (enterPassword.length() < 8 || enterPassword.length() > 15);
+        }
+        System.out.printf("%s, ваш новый пароль: %s", enterName, enterPassword);
+    }
+}
